7034e78dd5557ef1283a23c5d1d8d622208e5739
bhavithareddy147-droi <bhavithareddy147@gmail.com>
2026-03-13 10:53:34 +0530

    Add files via upload

---
+++ uppercase.js
@@ -0,0 +1,3 @@
+const words = ["hello", "world"];
+const shouted= words.map(word => word.toUpperCase());
+console.log(shouted);
---
+++ wordlen.js
@@ -0,0 +1,3 @@
+const words = ["apple","banana","kiwi","strawberry","orange"];
+const longwords= words.filter(word => word.length > 5);
+console.log(longwords);
